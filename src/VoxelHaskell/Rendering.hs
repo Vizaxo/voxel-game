816c72affe262cbb1991ce62589697f43132fc4e
Vizaxo <vizaxo@mailfence.com>
2019-08-13 03:27:40 +0100

    Add perspective viewing, movement/rotation, and coloured vertices

--- src/VoxelHaskell/Rendering.hs
+++ src/VoxelHaskell/Rendering.hs
@@ -3,14 +3,17 @@ module VoxelHaskell.Rendering where
 import Control.Applicative
 import Control.Monad.State
 import Control.Lens
+import Data.Distributive (distribute)
 import qualified Data.Map as M
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import qualified Data.Vector.Storable as V
+import Linear
 import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (Vector3(..), Color3(..), Vertex3(..), Color4(..), ($=))
+import qualified Graphics.GL as GL
 import qualified Graphics.UI.GLFW as GLFW
-import Foreign (sizeOf, nullPtr)
+import Foreign (sizeOf, nullPtr, castPtr, plusPtr, with, Ptr)
 
 import VoxelHaskell.Block
 import VoxelHaskell.GameState
@@ -19,7 +22,6 @@ import VoxelHaskell.World
 data RenderState = RenderState
   { _vao :: GL.VertexArrayObject
   , _vbo :: GL.BufferObject
-  , _vertexAttribute :: GL.AttribLocation
   , _shaderProg :: GL.Program
   }
 makeLenses ''RenderState
@@ -45,7 +47,6 @@ initOGL = do
   vao <- GL.genObjectName
   vbo <- GL.genObjectName
 
-  let vertexAttribute = GL.AttribLocation 0
   GL.bindVertexArrayObject $= Just vao
 
   vertexShader <- GL.createShader GL.VertexShader
@@ -54,18 +55,23 @@ initOGL = do
   GL.shaderSourceBS vertexShader $= Text.encodeUtf8
     (Text.pack $ unlines
       [ "#version 330 core"
-      , "layout (location = 0) in vec3 aPos"
+      , "uniform mat4 projection;"
+      , "layout (location = 0) in vec3 aPos;"
+      , "layout (location = 1) in vec4 colour;"
+      , "out vec4 vertexColour;"
       , "void main(void) {"
-      , "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+      , "  gl_Position = projection * vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+      , "  vertexColour = colour;"
       , "}"
       ])
 
   GL.shaderSourceBS fragmentShader $= Text.encodeUtf8
     (Text.pack $ unlines
       [ "#version 330"
-      , "out vec4 fragColor;"
+      , "out vec4 FragColor;"
+      , "in vec4 vertexColour;"
       , "void main(void) {"
-      , "  fragColor = vec4(1.0,1.0,1.0,1.0);"
+      , "  FragColor = vertexColour;"
       , "}"
       ])
 
@@ -75,11 +81,10 @@ initOGL = do
   shaderProg <- GL.createProgram
   GL.attachShader shaderProg vertexShader
   GL.attachShader shaderProg fragmentShader
-  GL.attribLocation shaderProg "aPos" $= vertexAttribute
   GL.linkProgram shaderProg
   GL.currentProgram $= Just shaderProg
 
-  pure (RenderState vao vbo vertexAttribute shaderProg)
+  pure (RenderState vao vbo shaderProg)
 
 vertices :: V.Vector Float
 vertices = packWorld $ renderWorld initialWorld
@@ -87,11 +92,6 @@ vertices = packWorld $ renderWorld initialWorld
 renderFrame :: (MonadState (GameState, RenderState) m, MonadIO m) => m ()
 renderFrame = do
   (gameState, renderState) <- get
-
-  liftIO $ GL.loadIdentity
-  liftIO $ GL.perspective 70 1 0.1 100
-  liftIO $ GL.rotate (gameState ^. playerAngle) (Vector3 0 1 (0 :: Float))
-  liftIO $ GL.translate (negate <$> gameState ^. playerPos)
 
   GL.clearColor $= Color4 0 0 0 0
   liftIO $ GL.clear [GL.ColorBuffer, GL.DepthBuffer]
@@ -103,11 +103,30 @@ renderFrame = do
     , v
     , GL.StaticDraw)
 
-  GL.vertexAttribPointer (renderState ^. vertexAttribute) $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 0 nullPtr)
-  GL.vertexAttribArray (renderState ^. vertexAttribute) $= GL.Enabled
+  let posAttribute = GL.AttribLocation 0
+      colourAttribute = GL.AttribLocation 1
 
+  GL.vertexAttribPointer posAttribute $=
+    (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float (fromIntegral (7 * sizeOf (0 :: Float))) nullPtr)
+  GL.vertexAttribArray posAttribute $= GL.Enabled
+
+  GL.vertexAttribPointer colourAttribute $=
+    (GL.ToFloat, GL.VertexArrayDescriptor 4 GL.Float (fromIntegral (7 * sizeOf (0 :: Float))) (plusPtr nullPtr (3 * sizeOf (0 :: Float))))
+  GL.vertexAttribArray colourAttribute $= GL.Enabled
+
+  let (Vector3 posX posY posZ) = gameState ^. playerPos
+  let projection = (perspective (90 / 180 * pi) 0.5 0.1 50 :: M44 GL.GLfloat)
+        !*! mkTransformation
+        (axisAngle (V3 0 1 0) (gameState ^. playerAngle / 180 * pi))
+        (V3 0 0 0)
+        !*! mkTransformation
+        (axisAngle (V3 0 1 0) 0)
+        (V3 (-posX) (-posY) (-posZ))
   GL.currentProgram $= Just (renderState ^. shaderProg)
+
+  GL.UniformLocation projectionLocation <- GL.get (GL.uniformLocation (renderState ^. shaderProg) "projection")
+  liftIO $ with (distribute $ projection) $ \ptr ->
+    GL.glUniformMatrix4fv projectionLocation 1 0 (castPtr ptr)
 
   liftIO $ GL.drawArrays GL.Triangles 0 (fromIntegral $ V.length vertices)
   liftIO $ GLFW.swapBuffers
@@ -115,24 +134,25 @@ renderFrame = do
 toFloat :: Integral n => n -> Float
 toFloat = fromIntegral
 
-packWorld :: [Vector3 Float] -> V.Vector Float
-packWorld = V.fromList . concatMap (\(Vector3 x y z) -> [x, y, z])
+packWorld :: [(Vector3 Float, Color4 Float)] -> V.Vector Float
+packWorld = V.fromList
+  . concatMap (\(Vector3 x y z, Color4 r g b a) -> [x, y, z, r, g, b, a])
 
-renderWorld :: World -> [Vector3 Float]
+renderWorld :: World -> [(Vector3 Float, Color4 Float)]
 renderWorld (World getChunk) =
-  --let chunksToRender = [Vector3 x y z | x <- [-1..1], y <- [-1..100], z <- [-1..1]]
-  let chunksToRender = [Vector3 0 (-1) 0]
+  let chunksToRender = [Vector3 x y z | x <- [-1..1], y <- [-1..100], z <- [-1..1]]
+  --let chunksToRender = [Vector3 x y z | ]
   in flip concatMap chunksToRender $ \pos ->
-    (liftA2 (+) (toFloat <$> ((* 16) <$> pos))) <$> renderChunk (getChunk pos)
+    over (mapped . _1) (liftA2 (+) (toFloat <$> ((* 16) <$> pos))) (renderChunk (getChunk pos))
 
-renderChunk :: Chunk -> [Vector3 Float]
+renderChunk :: Chunk -> [(Vector3 Float, Color4 Float)]
 renderChunk (Chunk (M.toList -> blocks)) =
   flip concatMap blocks $ \(pos, block) ->
-    (liftA2 (+) (toFloat <$> pos)) <$> renderBlock pos block
+    over (mapped . _1) (liftA2 (+) (toFloat <$> pos)) (renderBlock pos block)
 
-renderBlock :: Vector3 Int -> Block -> [Vector3 Float]
+renderBlock :: Vector3 Int -> Block -> [(Vector3 Float, Color4 Float)]
 renderBlock pos block =
-  [ Vector3 (-0.5) (-0.5) 0.5
-  , Vector3 0.5 (-0.5) 0.5
-  , Vector3 0.5 0.5 0.5
+  [ (Vector3 (-0.5) (-0.5) 0.5, Color4 1 0 0 1)
+  , (Vector3 0.5 (-0.5) 0.5, Color4 0 1 0 1)
+  , (Vector3 0.5 0.5 0.5, Color4 0 0 1 1)
   ]
